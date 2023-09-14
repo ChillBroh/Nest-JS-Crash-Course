3448af1432f85e6e9597afac7e7be476ee69f03b
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-09-15 00:06:45 +0530

    request validated and saved in local db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
-  signUp() {
-    return { msg: 'I am signed Up' };
+  constructor(private prisma: PrismaService) {}
+  async signUp(dto: AuthDto) {
+    //generate password hash
+    const hash = await argon.hash(dto.password);
+
+    //save new user into db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    //return user
+    return user;
   }
   signIn() {
     return { msg: 'I am signed in' };
