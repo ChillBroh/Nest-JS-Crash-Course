1d93ea5b5475f719ddddc7b3967b40c246b381b0
Ishara Madusanka <isharamadusanka410@gmail.com>
2023-09-14 17:18:10 +0530

    prisma dependency injected to autth module

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Module({
   providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class PrismaModule extends PrismaClient {
   constructor() {
